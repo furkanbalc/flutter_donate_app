f0807875519ed272ee711b11c65c368ef2b74346
Furkan Balcı <furkanbalc1903@gmail.com>
2024-05-25 21:49:13 +0300

    Custom animated snackbar added.

--- lib/core/utils/utils.dart
+++ lib/core/utils/utils.dart
@@ -1,126 +1,93 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_donate_app/presentation/widgets/progress/custom_loading_widget.dart';
+import 'package:flutter_donate_app/core/utils/custom_animated_snackbar.dart';
 
 class Utils {
   Utils._();
 
-  static errorSnackBar({required String mesaj, required BuildContext context, Function()? function}) {
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Material(
-            borderRadius: BorderRadius.circular(20),
-            elevation: 10,
-            shadowColor: Colors.red.shade600,
-            child: GestureDetector(
-              onTap: function,
-              child: Container(
-                height: 80,
-                alignment: Alignment.center,
-                padding: const EdgeInsets.all(20),
-                decoration: BoxDecoration(color: Colors.red.shade600, borderRadius: BorderRadius.circular(20)),
-                width: double.infinity,
-                child: Text(
-                  mesaj,
-                  style: const TextStyle(
-                    fontWeight: FontWeight.w400,
-                    fontSize: 12,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
-          ),
-          elevation: 0,
-          backgroundColor: Colors.transparent,
-        ),
-      );
-    });
-    ScaffoldMessenger.of(context).hideCurrentSnackBar(reason: SnackBarClosedReason.hide);
-  }
-
-  static successSnackBar({required String mesaj, required BuildContext context, Function()? function}) {
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Material(
-            borderRadius: BorderRadius.circular(20),
-            elevation: 10,
-            shadowColor: Colors.green.shade600,
-            child: GestureDetector(
-              onTap: function,
-              child: Container(
-                height: 80,
-                alignment: Alignment.center,
-                padding: const EdgeInsets.all(20),
-                decoration: BoxDecoration(color: Colors.green.shade600, borderRadius: BorderRadius.circular(20)),
-                width: double.infinity,
-                child: Text(
-                  mesaj,
-                  style: const TextStyle(
-                    fontWeight: FontWeight.w400,
-                    fontSize: 12,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
-          ),
-          elevation: 0,
-          backgroundColor: Colors.transparent,
-        ),
-      );
-    });
-    ScaffoldMessenger.of(context).hideCurrentSnackBar(reason: SnackBarClosedReason.hide);
-  }
-
-  static infoSnackBar({required String mesaj, required BuildContext context, Function()? function}) {
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Material(
-            borderRadius: BorderRadius.circular(20),
-            elevation: 10,
-            shadowColor: Colors.brown.shade900,
-            child: GestureDetector(
-              onTap: function,
-              child: Container(
-                height: 80,
-                alignment: Alignment.center,
-                padding: const EdgeInsets.all(20),
-                decoration: BoxDecoration(color: Colors.brown.shade900, borderRadius: BorderRadius.circular(20)),
-                width: double.infinity,
-                child: Text(
-                  mesaj,
-                  style: const TextStyle(
-                    fontWeight: FontWeight.w400,
-                    fontSize: 12,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
-          ),
-          elevation: 0,
-          backgroundColor: Colors.transparent,
-        ),
-      );
-    });
-    ScaffoldMessenger.of(context).hideCurrentSnackBar(reason: SnackBarClosedReason.hide);
-  }
-
-  static showLoadingDialog({
+  static void successSnackBar({
     required BuildContext context,
-  }) async {
-    return await showDialog(
-      barrierDismissible: false,
+    String? title,
+    required String message,
+    Function()? onTap,
+  }) {
+    _showAnimatedSnackbar(
       context: context,
+      message: message,
+      title: title,
+      backgroundColor: Colors.green.withOpacity(.7),
+      onTap: onTap,
+    );
+  }
+
+  static void errorSnackBar({
+    required BuildContext context,
+    String? title,
+    required String message,
+    Function()? onTap,
+  }) {
+    _showAnimatedSnackbar(
+      context: context,
+      message: message,
+      title: title,
+      backgroundColor: Colors.red.withOpacity(.7),
+      onTap: onTap,
+    );
+  }
+
+  static void infoSnackBar({
+    required BuildContext context,
+    String? title,
+    required String message,
+    Function()? onTap,
+  }) {
+    _showAnimatedSnackbar(
+      context: context,
+      message: message,
+      title: title,
+      backgroundColor: Colors.orange.withOpacity(.7),
+      onTap: onTap,
+    );
+  }
+
+  static final List<OverlayEntry> _overlayEntries = [];
+
+  static void _showAnimatedSnackbar({
+    required BuildContext context,
+    String? title,
+    required String message,
+    Function()? onTap,
+    required Color backgroundColor,
+  }) {
+    /// Eğer bir snackbar zaten gösteriliyorsa, ilk snackbar'ı kaldır ve yeni snackbar'ı ekle
+    if (_overlayEntries.isNotEmpty) {
+      _overlayEntries.first.remove();
+      _overlayEntries.clear();
+    }
+    final overlayEntry = OverlayEntry(
       builder: (context) {
-        return const Dialog.fullscreen(
-          backgroundColor: Colors.transparent,
-          child: CustomLoadingWidget(),
+        return Positioned(
+          left: 8,
+          right: 8,
+          child: AnimatedSnackbar(
+            title: title,
+            message: message,
+            onTap: onTap,
+            backgroundColor: backgroundColor,
+          ),
         );
       },
     );
+
+    _hideSnackBar(context, overlayEntry);
+  }
+
+  static void _hideSnackBar(BuildContext context, OverlayEntry overlayEntry) {
+    Overlay.of(context).insert(overlayEntry);
+    _overlayEntries.add(overlayEntry);
+
+    Future.delayed(const Duration(seconds: 4), () {
+      overlayEntry.remove();
+      _overlayEntries.remove(overlayEntry);
+    });
   }
 }
